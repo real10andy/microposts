5a2819124352ae4f62c86d2897d3cc8b16e25660
hiroto <hiro.pro1128@gmail.com>
2019-07-25 08:29:54 +0000

    micropost1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users=User.order(id: :desc).page(params[:page]).per(25)
@@ -39,7 +39,14 @@ class UsersController < ApplicationController
      counts(@user)
   end
   
-  private
+  def likes
+    @user=User.find(params[:id])
+    @microposts=@user.microposts.order(id: :desc).page(params[:page])
+    @fav_microposts=@user.fav_microposts.page(params[:page])
+    counts(@user)
+  end
+  
+  private   
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :reverses_of_favorite, class_name: "Favorite", foreign_key: "micropost_id"
+  has_many :users, through: :reverses_of_favorite
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
